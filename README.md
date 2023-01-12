6cd84bd54b7d74fdd23cd8a3739843bfe791ab09
Admin <admin@cap.ru>
2023-01-12 16:18:26 +0300

    дополнено

---
+++ README.md
@@ -0,0 +1,1 @@
+# kandomere.github.io
---
+++ text.py
@@ -0,0 +1,8 @@
+
+for i in range(1,8):
+    print(f"""<div class="col-sm-6 col-md-4 p-1 d-flex justify-content-center">
+                    <a class="lightbox" href="img/categories/Парящие/{i}par.jpg">
+                        <img src="img/categories/Парящие/{i}par.jpg" alt="Световые линии" style='width:300px; height:300px'>
+                    </a>
+                </div>""")
+
